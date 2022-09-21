6d084e1ba48725d8d818ced23793e5bb9b9c65c6
jinju9553 <69393506+jinju9553@users.noreply.github.com>
2022-09-21 19:14:26 +0900

    220921 commit

---
+++ Week11/src/N3969/Q2146.java
@@ -0,0 +1,144 @@
+package N3969;
+
+import java.io.*;
+import java.util.*;
+import java.awt.*;
+
+public class Q2146 {
+
+	public static int[][] island;
+	public static int[][] border;
+	public static boolean[][] visited;
+	
+	public static int borderIdx = 1;
+	public static int min = Integer.MAX_VALUE;
+	
+	public static int[] dx = {0, 0, 1, -1}; //대각선 이동 X
+	public static int[] dy = {1, -1, 0, 0};
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+		int N = Integer.parseInt(br.readLine());
+		
+		island = new int[N][N];
+		border = new int[N][N];
+		visited = new boolean[N][N];
+		
+		StringTokenizer st;
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			for(int j = 0; j < N; j++)
+				island[i][j] = Integer.parseInt(st.nextToken());
+		}
+		
+		//1.같은 대륙끼리 구별하기
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < N; j++) {
+				if(island[i][j] == 1) //땅이 있다면
+					borderBFS(i, j, N);
+			}
+		}
+		
+		visited = new boolean[N][N]; //초기화
+		
+		//2.최소 다리 길이 재기
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < N; j++) {
+				if(island[i][j] == 1) //땅이 있다면
+					findBridgeBFS(i, j, N, border[i][j]);
+			}
+		}
+		
+		//문제: (4, 3)에서 최소길이 3이 나올텐데 1까지만 세고 함수가 종료됨. 어디서 return 시키나??
+		
+		System.out.println(min);
+	}
+	
+	public static void borderBFS(int row, int col, int N) {
+		Queue<Point> queue = new LinkedList<>();
+		
+		if(border[row][col] != 0) //시작점이 이미 탐색 완료한 대륙 한가운데라면
+			return;
+		
+		visited[row][col] = true;
+		queue.add(new Point(col, row));
+		
+		if(island[row][col] == 1)
+			border[row][col] = borderIdx;
+		
+		while(!queue.isEmpty()) {
+			int size = queue.size();
+			
+			while(size-- > 0) { //사이즈가 왜 15000???
+				Point p = queue.poll();
+				
+				int prow = 0; int pcol = 0;
+				for(int i = 0; i < dx.length; i++) {
+					pcol = p.x + dx[i];
+					prow = p.y + dy[i];
+					
+					if(pcol > -1 && prow > -1 && pcol < N && prow < N) {
+						if(!visited[prow][pcol] && island[prow][pcol] == 1) {
+							visited[prow][pcol] = true;
+							border[prow][pcol] = borderIdx;
+							queue.add(new Point(pcol, prow));
+						}
+					}
+				} //좌표 더하기 끝
+			} //한 레벨 끝
+			
+			if(!isBorderLine(queue)) { //큐에 1이 없음 => 0밖에 없음 => 한 대륙 탐색 완료
+				borderIdx++;
+				return; //함수 종료
+			}
+		}
+	}
+	
+	public static void findBridgeBFS(int row, int col, int N, int borderNum) {
+		Queue<Point> queue = new LinkedList<>();
+		int count = 0;
+		
+		visited[row][col] = true;
+		queue.add(new Point(col, row));
+		
+		while(!queue.isEmpty()) {
+			int size = queue.size();
+			
+			while(size-- > 0) { //사이즈가 왜 15000???
+				Point p = queue.poll();
+				
+				int prow = 0; int pcol = 0;
+				for(int i = 0; i < dx.length; i++) {
+					pcol = p.x + dx[i];
+					prow = p.y + dy[i];
+					
+					if(pcol > -1 && prow > -1 && pcol < N && prow < N) {
+						if(!visited[prow][pcol] && border[prow][pcol] != borderNum) {
+							//방문한 적 없고, 같은 대륙이 아닐 때(바다 포함)
+							if(border[prow][pcol] == 0) { //바다라면
+								visited[prow][pcol] = true;
+								queue.add(new Point(pcol, prow));
+							}
+							else { //다른 대륙이라면 min 갱신
+								min = (count < min) ? count : min;
+								System.out.println(row + ", " + col);
+								return; //종료
+							}
+						}
+					}
+				} //좌표 더하기 끝
+			} //한 레벨 끝
+			count++;
+		}
+	}
+	
+	public static boolean isBorderLine(Queue<Point> queue) {
+		for(Point p : queue) {
+			if(island[p.y][p.x] == 1) //하나라도 1이라면
+				return true;
+		}
+		
+		return false;
+	}
+}
--- Week11/src/N3972/Q1339_2.java
+++ Week11/src/N3972/Q1339_2.java
@@ -1,22 +1,72 @@
 package N3972;
 
+import java.io.*;
+import java.util.*;
+
 public class Q1339_2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		//pick함수를 이용해서 푼다. (입력이 작아서 가능할듯)
-	}
-	
-	public static void pick(int[] items, int[] bucket, int M, int k, int lastIndex) {
-		if(M == k) {
-			//bucket에 담긴 숫자 순서대로 단어를 계산해본다.
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		String[] words = new String[N];
+		int[] numbers = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+		Map<Character, Integer> map = new HashMap<>();
+		
+		//1.입력 문자열 읽기
+		int maxLen = Integer.MIN_VALUE;
+		for(int i = 0; i < N; i++) {
+			words[i] = br.readLine();
+			int len = words[i].length();
+			maxLen = (len > maxLen) ? len : maxLen;
 		}
 		
-		for(int i = 0; i < items.length; i++) {
-			//items 배열에 9부터 담아서, 내림차순부터 오름차순으로 찾아나간다.
-			//9876543210, 9876543201, ... 
+		//2.matrix 초기화
+		char[][] matrix = new char[N][maxLen];
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < maxLen; j++)
+				matrix[i][j] = 'x';
 		}
+		
+		//3.문자열 자릿수에 맞게 matrix 채워넣기 (2번과 합칠 수 있음)
+		for(int i = 0; i < N; i++) {
+			int gap = maxLen - words[i].length();
+			for(int j = words[i].length() - 1; j >= 0; j--) {
+				matrix[i][j + gap] = words[i].charAt(j);
+			}
+		}
+		
+		//4.map을 이용해 다항식 계수 구하기
+		for(int j = 0; j < maxLen; j++) { //세로로 한 줄씩 탐색
+			for(char c : map.keySet()) { //다음 자릿수로 넘어갈 때마다 자리 올림 수행
+				int n = map.get(c);
+				map.put(c, n * 10);
+			}
+			
+			for(int i = 0; i < N; i++) { //세로 한 줄의 한 칸씩 
+				if(matrix[i][j] != 'x' && !map.containsKey(matrix[i][j])) { 
+					//비어있지 않고, map에 없다면
+					map.put(matrix[i][j], 1);
+				}
+				else if(map.containsKey(matrix[i][j])) { //map에 있다면
+					int n = map.get(matrix[i][j]);
+					map.put(matrix[i][j], n + 1); //계수 추가
+				}
+			}
+		}
+		
+		//5.계수만 따로 빼기(Comparator를 따로 구현할 수 없음)
+		Integer[] arr = new Integer[map.values().size()]; int idx = 0;
+		for(int v : map.values())
+			arr[idx++] = v;
+		
+		//6.합계 출력
+		int sum = 0;
+		Arrays.sort(arr, Collections.reverseOrder());
+		for(int i = 0; i < arr.length; i++)
+			sum += arr[i] * numbers[i];
+		
+		System.out.println(sum);
 	}
-
 }
