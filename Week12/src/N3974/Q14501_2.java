1cac4d90b4781142cc1c260313a5ad8bda455baa
jinju9553 <69393506+jinju9553@users.noreply.github.com>
2022-10-03 16:22:18 +0900

    221003 commit

--- Week12/src/N3974/Q14501_2.java
+++ Week12/src/N3974/Q14501_2.java
@@ -1,6 +1,7 @@
 package N3974;
 
 import java.io.*;
+import java.util.Arrays;
 
 public class Q14501_2 {
 
@@ -13,14 +14,24 @@ public class Q14501_2 {
 		int[] DP = new int[N + 1];
 		
 		String[] temp;
-		for(int i = 1; i < N + 1; i++) {
+		for(int i = 0; i < N; i++) {
 			temp = br.readLine().split(" ");
 			times[i] = Integer.parseInt(temp[0]);
 			pays[i] = Integer.parseInt(temp[1]);
 			DP[i] = 0;
 		}
 		
-		//
+		for(int i = 0; i < N; i++) { // i < 10 이라면 i = 1~9
+			if(i + times[i] < N + 1) { //N일째까지는 가능
+				int next = i + times[i];
+				DP[next] = Math.max(pays[i] + DP[i], DP[next]);
+			}
+			DP[i + 1] = Math.max(DP[i + 1], DP[i]);
+		}
+		
+		Arrays.sort(DP);
+
+		System.out.println(DP[N]);
 	}
 
 }
